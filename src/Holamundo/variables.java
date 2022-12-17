d1d2cd5a655cd461a8c0aaec9f7f16d9acaafe64
paul <videoblogpaul@gmail.com>
2022-12-17 22:01:49 +0100

    Initial commit

---
+++ src/Holamundo/variables.java
@@ -0,0 +1,14 @@
+package Holamundo;
+
+public class variables {
+    public static void main(String[] args) {
+        int speedy;
+        speedy = 10;
+        System.out.println(speedy);
+
+        int salary = 1000;
+        String employe = "PAul Vargas";
+
+        System.out.println(employe);
+    }
+}
